e4585779d4516badf2c91890c56d63f5e64e748e
Andi Alfi Syahrin <andi.alfi.syahrin@mail.ugm.ac.id>
2022-05-24 20:13:19 +0800

    commit on 'master'

--- router/auth.js
+++ router/auth.js
@@ -9,30 +9,6 @@ router.get('/', (req, res) => {
     res.send('Hello World from the server router')
 }) 
 
-// router.post('/register', (req, res) => {
-//     const { name, email, phone, work, password, cpassword } = req.body
-
-//     if ( !name || !email || !phone || !work || !password || !cpassword ) {
-//         return res.status(422).json({ error: "Plz filled the field properly "})
-//     }
-
-//     User.findOne({ email: email })
-//         .then((userExist) => {
-//             if (userExist) {
-//                 return res.status(422).json({ error: 'Email already exist' })
-//             }
-
-//             const user = new User({ name, email, phone, work, password, cpassword })
-
-//             user.save().then(() => {
-//                 res.status(201).json({ message: "user registered successfully" })
-//             }).catch((err) => res.status(500).json({ error: "Failed to registered"}))
-//         }).catch(err => {
-//             console.log(err)
-//         })
-// })
-//
-
 router.post('/register', async (req, res) => { 
     const { name, email, phone, work, password, cpassword } = req.body
     if ( !name || !email || !phone || !work || !password || !cpassword ) {
